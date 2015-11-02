98ebba8761fe8d46e7fb89e5cdf78d30bc992071
fmonehin <femi.monehin@settle.com.ng>
2015-11-02 17:02:47 +0100

    1st commit

---
+++ gitTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  gitTest
+//
+//  Created by Olufemi Monehin on 11/2/15.
+//  Copyright © 2015 SettleMe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Interesting GIT")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
